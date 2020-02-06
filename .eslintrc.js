a697d0cc4bcc328b4ecc605de09909f9c3275e48
kimtuan1102 <kimtuan1102@gmail.com>
2020-02-06 18:14:59 +0700

    remove project

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: 'plugin:vue/recommended',
-  // add more generic rulesets here, such as:
-  rules: {
-    // override/add rules settings here, such as:
-    'vue/no-unused-vars': 'error',
-  },
-};
