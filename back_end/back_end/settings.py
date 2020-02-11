fc5be10f4cfda1e1a972e3ab35ea78408f4d1830
tuantk <kimtuan1102@gmail.com>
2020-02-11 10:44:35 +0700

    add model product and categories

--- back_end/back_end/settings.py
+++ back_end/back_end/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'back_end.trade_shop',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),  # Or an IP Address that your DB is hosted on
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
